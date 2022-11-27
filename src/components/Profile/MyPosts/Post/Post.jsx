f99782f52c640c0b43e42393d84011048ba50edd
nayfort <115005873+nayfort@users.noreply.github.com>
2022-11-27 23:03:36 +0100

    reduxstart

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -8,7 +8,7 @@ const Post = (props) => {
                 src='https://media.gq-magazine.co.uk/photos/62c40d1d9096f9cf3dec0365/16:9/w_1280,c_limit/Jake_Flying_ATWOW.jpeg'/>
             {props.message}
             <div>
-                <span>like</span> { props.likesCount }
+                <span>like</span> {props.likesCount}
             </div>
         </div>
 
