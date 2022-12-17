bcd9352bbbc97bc6c1ab73f55703e23ed31c5c57
nayfort <115005873+nayfort@users.noreply.github.com>
2022-12-17 15:47:16 +0100

    51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,37 +1,14 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taras_Shevchenko_selfportrait_oil_1840.jpg/175px-Taras_Shevchenko_selfportrait_oil_1840.jpg',
-                    followed: false,
-                    fullName: 'Vladislav',
-                    status: 'Boss',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taras_Shevchenko_selfportrait_oil_1840.jpg/175px-Taras_Shevchenko_selfportrait_oil_1840.jpg',
-                    followed: true,
-                    fullName: 'Alex',
-                    status: 'Fullstack',
-                    location: {city: 'New York', country: 'USA'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taras_Shevchenko_selfportrait_oil_1840.jpg/175px-Taras_Shevchenko_selfportrait_oil_1840.jpg',
-                    followed: false,
-                    fullName: 'Patrik',
-                    status: 'Middle',
-                    location: {city: 'Brno', country: 'Czech Republic'}
-                },
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        })
     }
 
     return <div>
@@ -39,7 +16,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small : ''} className={styles.userPhoto}/>
                 </div>
                 <div>
                     {u.followed
@@ -53,11 +30,11 @@ let Users = (props) => {
             </span>
                 <span>
                 <span>
-                    <div>{u.fullName}</div><div>{u.status}</div>
+                    <div>{u.name}</div><div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{'u.location.country'}</div>
+                    <div>{'u.location.city'}</div>
                 </span>
             </span>
             </div>)
