2ec1fddcce3264d4d001d0cdc9dcc2dc80f1ebd5
nayfort <115005873+nayfort@users.noreply.github.com>
2022-12-23 16:01:47 +0100

    isFetching57

---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "../../Users/assets/images/loader.gif";
+import React from "react";
+
+let Preloader = (props) => {
+    return <div><img src={preloader}/></div>
+}
+
+export default Preloader;
