daf29ab35670354787bcf3ce58640f08f490c42f
nayfort <115005873+nayfort@users.noreply.github.com>
2022-12-26 18:26:22 +0100

    61.isAuth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
-
-import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
-                <Header/>
+                <HeaderContainer/>
                 <NavBar/>
                 <div className='app-wrapper-content'>
                         <Route path="/profile/:userId?" render={() => <ProfileContainer />}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import classes from './Header.module.css'; 
+import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={classes.header}>
       <img src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg"></img>
+
+        <div className={classes.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+        </div>
     </header>
   );
 };
