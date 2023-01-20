0bcfedb2094d4005b5926fb6a96d8c8aa91b0e15
nayfort <mitrofanovvladislav2004@gmail.com>
2023-01-20 16:00:39 +0100

    92.profileTests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/redux-store";
 import React from "react";
 import './index.css'
-import App from "./App";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import MainApp from './App';
 
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>, document.getElementById('root')
-    );
+    ReactDOM.render(<MainApp />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
